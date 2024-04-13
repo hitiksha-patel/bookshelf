b6b45f81f0d6674c38e2c22163c8c7da54b292d5
hitiksha-patel <hitikshapatel23@gmail.com>
2024-04-13 01:46:12 -0400

    added mongodb connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001
+const db = process.env.DATABASE
 
-app.get('/', (req, res) => {
-  res.send('Bookshelf is running!!!')
-})
+//Database connection - MongoDB
+
+try {
+  mongoose.connect(db, {
+     useNewUrlParser: true,
+     useUnifiedTopology: true,
+  })
+   console.log("Connected to MongoDB");
+}catch(err) {
+   console.log("Error connecting to MongoDB:", err);
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
