b19e6ea355f89c4be68fbe31e3a4d337bdb39706
hitiksha-patel <hitikshapatel23@gmail.com>
2024-04-20 15:14:34 -0400

    modified app.js

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import Home from "./home/Home.jsx";
-import Courses from "./courses/Courses.jsx";
-
+import Home from "./home/Home";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Signup from "./components/Signup.jsx";
-import toast, { Toaster } from "react-hot-toast";
-import { useAuth } from "./context/AuthProvider.jsx";
+import Courses from "./courses/Courses";
+import Signup from "./components/Signup";
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./context/AuthProvider";
 
 function App() {
   const [authUser, setAuthUser] = useAuth();
   console.log(authUser);
-
   return (
     <>
       <div className="dark:bg-slate-900 dark:text-white">
